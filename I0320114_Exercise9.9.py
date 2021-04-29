103518f57c6fd356bad27b7e3c3591b421c7dd74
Yukuri Hanjani <hanjaniyukuri@gmail.com>
2021-04-29 22:39:49 +0700

    Menambah exercise 9.10 ke dalam satu file dengan exercise 9.9

--- I0320114_Exercise9.9.py
+++ I0320114_Exercise9.9.py
@@ -7,3 +7,10 @@ print(A)
 A[1] = -700 # Mengubah elemen kedua
 A[4] = 800 # Mengubah elemen kelima
 print(A)
+
+# exercise 9.10
+# Membalik urutan Array
+A.reverse()
+
+# Nilai akhir setelah dibalik
+print(A)
